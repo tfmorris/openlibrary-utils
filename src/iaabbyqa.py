be6603aa300af54720d795bb87a71ffee6d0fed0
Tom Morris <tfmorris@gmail.com>
2013-12-16 14:34:17 -0500

    Final version for Nov 2013 run. Add exception handling & more stats.  Process all files in directory.

--- src/iaabbyqa.py
+++ src/iaabbyqa.py
@@ -4,6 +4,8 @@ Prototype to tally ABBY OCR stats as recorded in XML file including:
 - in dictionary word percentage
 - average word penalty
 '''
+from datetime import datetime
+import glob
 import gzip
 import lxml.etree as ET
 from xml.etree import cElementTree
@@ -13,9 +15,8 @@ HEADER = ['<?xml version="1.0" encoding="UTF-8"?>',
 #    '<document version="1.0" producer="LuraDocument XML Exporter for ABBYY FineReader" pagesCount="1"',
 #    'xmlns="http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml">',
     ]
-FILENAME = '../pilgrimsprogr1796buny_abbyy.gz'
 
-
+CACHE_DIR = '../cache/'
 
 def processfile(filename):
     localfile =filename
@@ -32,63 +33,71 @@ def processfile(filename):
     penalty_count = 0
     conf_tot = 0
     with gzip.open(localfile, 'rb') as f:
-        for line in f:
-                linenum += 1
-                if line.startswith('<page'):
-                    pagenum += 1
-                    xml = list(HEADER)
-                    while not line.startswith('</page'):
+        try:
+            for line in f:
+                    linenum += 1
+                    if line.startswith('<page'):
+                        pagenum += 1
+                        xml = list(HEADER)
+                        while not line.startswith('</page'):
+                            xml.append(line)
+                            linenum += 1
+                            line = f.next()
                         xml.append(line)
-                        linenum += 1
-                        line = f.next()
-                    xml.append(line)
-            #        xml.append('</document>')
-                    
+                #        xml.append('</document>')
 
-                    dom = ET.fromstring('\n'.join(xml))
-                    chars = dom.findall('.//charParams')
-                    for c in chars:
-                        s = ''
-                        conf = int(c.attrib['charConfidence'])
-                        if conf<255: # spaces have a confidence of 255 and can be skipped
-                            conf_tot = conf_tot + conf
-                            charcount += 1
-                        if 'suspicious' in c.attrib:
-                            susp = c.attrib['suspicious']
-                            susp_count += 1
-                        start = c.attrib['wordStart']
+                        dom = ET.fromstring('\n'.join(xml))
+                        chars = dom.findall('.//charParams')
+                        for c in chars:
+                            s = ''
+                            conf = int(c.get('charConfidence',255))
+                            if conf<255: # spaces have a confidence of 255 and can be skipped
+                                conf_tot = conf_tot + conf
+                                charcount += 1
+                            susp = c.get('suspicious',False)
+                            if susp:
+                                susp_count += 1
+                            start = c.get('wordStart', False)
 
-                        # Word attributes - these should stay constant for whole word
-                        dic = c.attrib['wordFromDictionary']
-                        penalty = c.attrib['wordPenalty']
-                        normal = c.attrib['wordNormal']
-                        if start == 'true':
-                            wordcount += 1
-                            wdic = dic
-                            if dic == 'true':
-                                dic_count += 1
-                            wpenalty = penalty
-                            if penalty and penalty != '0':
-                                penalty_tot += int(penalty)
-                                penalty_count += 1
-                            wnormal = normal
-                            #print ''
-                        elif penalty != '0':
-                            assert wdic == dic
-                            assert wpenalty == penalty
-                            assert wnormal == normal
-                            #print '\t'.join([str(conf),susp,dic,penalty,c.text])
-        avg_word_penalty = 0
-        pct_in_dict = 0
-        if wordcount:
-            avg_word_penalty = penalty_tot*1.0/wordcount
-            pct_in_dict = dic_count * 100.0 / wordcount
-        avg_char_confidence = 0
-        if charcount:
-            avg_char_confidence = conf_tot * 1.0 / charcount
-        print "%d pages, %d words, %5.2f%% in dict, %5.2f%% penalties, %5.2f char/word, %5.2f avg word penalty, %5.2f char conf"% (pagenum,wordcount,pct_in_dict, penalty_count*100.0/wordcount,charcount*1.0/wordcount, avg_word_penalty, avg_char_confidence)
+                            # Word attributes - these should stay constant for whole word
+                            dic = c.get('wordFromDictionary', False)
+                            penalty = c.get('wordPenalty', False)
+                            normal = c.get('wordNormal', False)
+                            if start == 'true':
+                                wordcount += 1
+                                wdic = dic
+                                if dic == 'true':
+                                    dic_count += 1
+                                wpenalty = penalty
+                                if penalty and penalty != '0':
+                                    penalty_tot += int(penalty)
+                                    penalty_count += 1
+                                wnormal = normal
+
+            avg_word_penalty = 0
+            avg_word_chars = 0
+            pct_in_dict = 0
+            penalty_pct = 0
+            if wordcount:
+                avg_word_penalty = penalty_tot*1.0/wordcount
+                pct_in_dict = dic_count * 100.0 / wordcount
+                avg_word_chars = charcount*1.0/wordcount
+                penalty_pct = penalty_count*100.0/wordcount
+            avg_char_confidence = 0
+            if charcount:
+                avg_char_confidence = conf_tot * 1.0 / charcount
+            print "%d pages, %d words, %5.2f%% in dict, %5.2f%% penalties, %5.2f char/word, %5.2f avg word penalty, %5.2f char conf" \
+                % (pagenum,wordcount,pct_in_dict, penalty_pct,avg_word_chars, avg_word_penalty, avg_char_confidence)
+
+        except IOError as e:
+            print  'Error decoding ',localfile,e
 
 def main():
-    processfile(FILENAME)
+    files = glob.glob(CACHE_DIR+'*_abbyy.gz')
+    print 'Found %d files' % len(files)
+    start = datetime.now()
+    for f in files:
+        processfile(f)
+    print 'Processed %d files in %s' % (len(files),str(datetime.now() -start))
 
 main()
